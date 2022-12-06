869d524bd089777231b478b018e2927cd4b26b45
Salehkhatib <skhatib552@gmail.com>
2022-12-06 10:06:30 +0100

    TodoVue

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
-Vue.createApp({
+const app = Vue.createApp({
   data() {
     return {
       newTodo: "",
-      todos: [
-        {
-          id: 1,
-          description: "Html lernen",
-          done: false,
-        },
-        {
-          id: 2,
-          description: "CSS lernen",
-          done: false,
-        },
-        {
-          id: 3,
-          description: "Vue.js lernen",
-          done: false,
-        },
-      ],
+      todos: [],
     };
   },
+
+  async created() {
+    const response = await fetch("http://localhost:3000/todos");
+    this.todos = await response.json();
+  },
   methods: {
+    async saveTodos() {
+      const newTodo = {
+        description: this.newTodo,
+        done: false,
+      };
+      await fetch("http://localhost:3000/todos", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newTodo),
+      });
+      this.addNewTodo();
+    },
+
+    async deleteTodos() {
+      this.todos.forEach((todo) => {
+        if (todo.done === true) {
+          let id = todo.id;
+          fetch("http://localhost:3000/todos/" + id, {
+            method: "DELETE",
+          })
+            .then((res) => res.json())
+            .then(() => {});
+        }
+      });
+      this.deleteAllDone();
+    },
+
     addNewTodo() {
       this.todos.push({
         id: Date.now(),
@@ -30,17 +48,49 @@ Vue.createApp({
       });
       this.resetInput();
     },
+
+    resetInput() {
+      this.newTodo = "";
+    },
+
     changeDoneState(id) {
       const currentTodo = this.todos.find((todo) => todo.id === id);
       currentTodo.done = !currentTodo.done;
     },
+
     deleteAllDone() {
       this.todos = this.todos.filter((todo) => {
         return todo.done !== true;
       });
     },
-    resetInput() {
-      this.newTodo = "";
+
+    filterDone(event) {
+      let filteredList = [];
+      if (event.target.checked) {
+        filteredList = this.todos.filter((todo) => {
+          if (todo.done === true) {
+            return todo;
+          }
+        });
+      }
+      this.todos = filteredList;
+    },
+
+    filterOpen(event) {
+      let filteredList = [];
+      if (event.target.checked) {
+        filteredList = this.todos.filter((todo) => {
+          if (todo.done === false) {
+            return todo;
+          }
+        });
+      }
+      this.todos = filteredList;
+    },
+
+    async loadTodos() {
+      const response = await fetch("http://localhost:3000/todos");
+      this.todos = await response.json();
     },
   },
 }).mount("#app");
